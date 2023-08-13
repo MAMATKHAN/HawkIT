e46b27629cfd4e249dcc2159f8dca11c16ebc65f
mamatkhan <mamat.magomednabiyev@gmail.com>
2023-08-13 15:27:21 +0300

    Admin panel v1.1

--- HawkIT/Controllers/AdminController.cs
+++ HawkIT/Controllers/AdminController.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HawkIT.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HawkIT.Controllers
 {
-    public class AuthenticationController : Controller
+    public class AdminController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            return RedirectToAction("Index", "Admin");
+        }
+
+        public IActionResult Index()
         {
             return View();
         }
