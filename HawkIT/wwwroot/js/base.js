ff5772a482916cd9b6de30b74b6d87668f138c89
123 <user@DESKTOP-VUSU881>
2023-08-14 20:06:43 +0300

    add more with banner + add base.js

---
+++ HawkIT/wwwroot/js/base.js
@@ -0,0 +1,17 @@
+﻿
+console.log('curre');
+let scroll = document.querySelector('.intro__mouse__scroll');
+console.log('curre');
+
+
+
+function animus() {
+    scroll.classList.add('intro__mouse__scroll__active');
+    setTimeout(function () {
+        scroll.classList.remove('intro__mouse__scroll__active');
+    }, 400)
+}
+
+
+
+setInterval(animus, 1200);
