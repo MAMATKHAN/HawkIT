7a242f28b8e188cdc2cd5bf37f7656c48512fdc1
123 <user@DESKTOP-VUSU881>
2023-08-15 21:28:46 +0300

    add pizdec kak dohyia

--- HawkIT/wwwroot/js/base.js
+++ HawkIT/wwwroot/js/base.js
@@ -1,4 +1,6 @@
 ﻿
+/*   анимация мышки   */
+
 console.log('curre');
 let scroll = document.querySelector('.intro__mouse__scroll');
 console.log('curre');
@@ -15,3 +17,78 @@ function animus() {
 
 
 setInterval(animus, 1200);
+
+
+
+let infoBtns = document.querySelectorAll(".info__line__btn");
+let infoServ = document.querySelector(".info__service");
+let infoTeam = document.querySelector(".info__team");
+let infoMiss = document.querySelector(".info__mission");
+
+console.log(infoMiss);
+
+infoBtns[0].addEventListener("click", function () {
+    infoBtns[1].classList.remove('info__line__btn__active');
+    infoBtns[2].classList.remove('info__line__btn__active');
+    infoBtns[0].classList.add('info__line__btn__active');
+
+    infoTeam.classList.remove('info__anime');
+    infoMiss.classList.remove('info__anime');
+    setTimeout(function () {
+        infoTeam.classList.remove('info__active');
+        infoMiss.classList.remove('info__active');
+    }, 200);
+
+    setTimeout(function () {
+        infoServ.classList.add('info__active');
+        setTimeout(function () {
+            infoServ.classList.add('info__anime');
+        }, 100);
+    }, 200);
+})
+
+
+
+infoBtns[1].addEventListener("click", function () {
+    infoBtns[0].classList.remove('info__line__btn__active');
+    infoBtns[2].classList.remove('info__line__btn__active');
+    infoBtns[1].classList.add('info__line__btn__active');
+
+    console.log(infoMiss);
+
+    infoServ.classList.remove('info__anime');
+    infoMiss.classList.remove('info__anime');
+    setTimeout(function () {
+        infoServ.classList.remove('info__active');
+        infoMiss.classList.remove('info__active');
+    }, 200);
+
+    setTimeout(function () {
+        infoTeam.classList.add('info__active');
+        setTimeout(function () {
+            infoTeam.classList.add('info__anime');
+        }, 100);
+    }, 200);
+})
+
+
+
+infoBtns[2].addEventListener("click", function () {
+    infoBtns[0].classList.remove('info__line__btn__active');
+    infoBtns[1].classList.remove('info__line__btn__active');
+    infoBtns[2].classList.add('info__line__btn__active');
+
+    infoServ.classList.remove('info__anime');
+    infoTeam.classList.remove('info__anime');
+    setTimeout(function () {
+        infoServ.classList.remove('info__active');
+        infoTeam.classList.remove('info__active');
+    }, 200);
+
+    setTimeout(function () {
+        infoMiss.classList.add('info__active');
+        setTimeout(function () {
+            infoMiss.classList.add('info__anime');
+        }, 100);
+    }, 200);
+})
