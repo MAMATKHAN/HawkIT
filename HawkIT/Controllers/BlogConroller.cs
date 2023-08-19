6bc491d9bf2919d75f436572ded120a4bc768734
mamatkhan <mamat.magomednabiyev@gmail.com>
2023-08-19 13:29:06 +0300

    Add BlogController

---
+++ HawkIT/Controllers/BlogConroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HawkIT.Controllers
+{
+    public class BlogConroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
