39b5e821e6bc858ab5561eccc6f61fc012fcbdfb
mamatkhan <mamat.magomednabiyev@gmail.com>
2023-08-19 12:18:15 +0300

    Admin panel 1.7(Project - Edit, Delete, Broese, Create)

--- HawkIT/Models/Project.cs
+++ HawkIT/Models/Project.cs
@@ -6,12 +6,12 @@ namespace HawkIT.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
         public DateTime CreatedDate { get; set; }
         public string Images { get; set; }
-        public List<Tag> Tags { get; set; }
+        public List<Tag>? Tags { get; set; }
         public List<Worker> Workers { get; set; }
         [NotMapped]
-        public IFormFile formFile { get; set; }
+        public IFormFile ImageFile { get; set; }
     }
 }
