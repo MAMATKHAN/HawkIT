cb15604595fc33dfe3bba1893af4afec25cefb78
mamatkhan <mamat.magomednabiyev@gmail.com>
2023-08-19 13:29:48 +0300

    rename BlogController action(Index -> ListItems)

--- HawkIT/Controllers/BlogConroller.cs
+++ HawkIT/Controllers/BlogConroller.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HawkIT.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HawkIT.Controllers
 {
     public class BlogConroller : Controller
     {
-        public IActionResult Index()
+        private readonly HawkitDbContext db;
+        public BlogConroller(HawkitDbContext context)
         {
+            db = context;
+        }
+
+        public IActionResult ListItems()
+        {
+            var articles = db.Articles.ToList();
             return View();
         }
     }
